a80217e04980a53bf2963f597c402829015af2bf
TheNewestNewbie <thenewestnewbie@gmail.com>
2023-08-25 11:51:50 -0500

    fix (register) : changed register to socket instance

--- serv/router/auth/register.js
+++ serv/router/auth/register.js
@@ -3,100 +3,103 @@ const uid = require(config.SERV + "/helpers/uid.js");
 const bcrypt = require("bcryptjs");
 const { User } = require(config.SERV + "/helpers/db.js");
 
-/* Funtion register
- * @params req{ body : {username, password , rpassword , token}}
- * @params res {}
- */
+const register = async (io, socket, id) => {
+    socket.on("register", async (data) => {
+        let username,
+            password,
+            rpassword;
 
-const register = async (req, res) => {
-
-    let username,
-        password,
-        rpassword,
-        token;
-
-    try {
-        const body = req.body;
-        username = (body.username ? body.username : undefined);
-        password = (body.password ? body.password : undefined);
-        rpassword = (body.rpassword ? body.rpassword : undefined);
-        token = (body.token ? body.token : undefined)
-    } catch (err) {
-        return res.json({
-            status: false,
-            data: "DATA_ERROR"
+        const admin = await User.findOne({
+            where: {
+                username: "admin"
+            }
         });
-    }
-
-    if (!username) {
-        return res.json({
+        if(!admin || admin.user_id != id) return socket.emit("register" , {
             status: false,
-            data: "EMPTY_USER"
+            data: "NO_PRIVILEGES"
         });
-    }
-    
-    if(username.length < 5){
-        return res.json({
-            status: false,
-            data: "USER_LENGTH"
-        })
-    }
 
-    else if (!password) {
-        return res.json({
-            status: false,
-            data: "EMPTY_PASS"
-        });
-    }
+        try {
+            username = (data.username ? data.username : undefined);
+            password = (data.password ? data.password : undefined);
+            rpassword = (data.rpassword ? data.rpassword : undefined);
+        } catch (err) {
+            return socket.emit("register", {
+                status: false, data: "DATA_ERROR",
+                error: err
+            })
+        }
 
-    if (await User.findOne({
+        if (!username) {
+            return socket.emit("register", {
+                status: false,
+                data: "EMPTY_USER"
+            });
+        }
+
+        if (username.length < 5) {
+            return socket.emit("register", {
+                status: false,
+                data: "USER_LENGTH"
+            })
+        }
+
+        else if (!password) {
+            return socket.emit("register", {
+                status: false,
+                data: "EMPTY_PASS"
+            });
+        }
+
+        if (await User.findOne({
             where: { username: username }
-        })) return res.json({ status: false, data: "ACC_USE" });
+        })) return socket.emit("register", { status: false, data: "ACC_USE" });
 
-    const char = /^[a-zA-Z0-9]+$/;
-    if (!char.test(username)) {
-        return res.json({
-            status: false,
-            data: "USERNAME_BAD_CHAR"
-        });
-    }
+        const char = /^[a-zA-Z0-9]+$/;
+        if (!char.test(username)) {
+            return socket.emit("register", {
+                status: false,
+                data: "USERNAME_BAD_CHAR"
+            });
+        }
 
-    if (password.length < 8) {
-        return res.json({
-            status: false,
-            data: "PASS_LENGTH"
-        });
-    }
+        if (password.length < 8) {
+            return socket.emit("register", {
+                status: false,
+                data: "PASS_LENGTH"
+            });
+        }
 
-    if (password != rpassword) {
-        return res.json({
-            status: false,
-            data: "PASS_NOT_MATCH"
-        });
-    }
+        if (password != rpassword) {
+            return socket.emit("register", {
+                status: false,
+                data: "PASS_NOT_MATCH"
+            });
+        }
 
 
-    try {
-        await User.create({
-            user_id: parseInt(uid.num(8)),
-            username: username,
-            password: bcrypt.hashSync(password, 10)
-        });
+        try {
+            await User.create({
+                user_id: parseInt(uid.num(8)),
+                username: username,
+                password: bcrypt.hashSync(password, 10)
+            });
 
-        return res.json({
-            status: true,
-            data: "REGISTERED"
-        });
+            return socket.emit("register", {
+                status: true,
+                data: "REGISTERED"
+            });
 
-    } catch (err) {
-        console.log(err);
-        return res.json({
-            status: false,
-            data: "DATA_ERROR",
-            error: err
-        });
+        } catch (err) {
+            console.log(err);
+            return socket.emit("register", {
+                status: false,
+                data: "DATA_ERROR",
+                error: err
+            });
 
-    }
+        }
+    });
 }
 
 
