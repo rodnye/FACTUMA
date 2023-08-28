c60feb103126daf87961a6d577842177d70d62a9
TheNewestNewbie <thenewestnewbie@gmail.com>
2023-08-27 20:52:20 -0500

    feat (serv/router): added cashier list to express router

--- serv/router/router.js
+++ serv/router/router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const auth = require("./auth/router.js"); 
+const {cashierListRest} = require("./cashier/cashier.js");
 
 router.use("/auth", auth); 
+router.get("/cashierList" , cashierListRest);
 
 module.exports = router;
